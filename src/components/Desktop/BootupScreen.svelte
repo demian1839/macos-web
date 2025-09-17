e0d9af5f53dfea75ca68d3e0cbb5390f7726c1b0
demian1839 <dggdemian@gmail.com>
2025-09-17 17:57:42 +0200

    Update BootupScreen.svelte

--- src/components/Desktop/BootupScreen.svelte
+++ src/components/Desktop/BootupScreen.svelte
@@ -2,8 +2,7 @@
 	import { onMount } from 'svelte';
 	import { quintInOut } from 'svelte/easing';
 	import { tweened } from 'svelte/motion';
-	import AppleIcon from '/public/app-icons
-/appstore';
+	import AppleIcon from '/public/app-icons/appstore';
 	import { elevation } from '🍎/actions';
 	import { fade_out } from '🍎/helpers/fade.ts';
 	import { sleep } from '🍎/helpers/sleep';
